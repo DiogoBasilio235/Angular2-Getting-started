061450c9f1e2693a14190fcdc4e0b91a4e30dcff
diogobasilio235 <diogobasilio235@gmail.com>
2022-12-21 17:55:54 +0000

    commit to save up until module 7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+//Component Decorator
+//
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
+  template: `<div><h1>{{pageTitle}}</h1>
+  <pm-products></pm-products>
+  </div>`, //Any valid html can be a template
   styleUrls: ['./app.component.css']
 })
+
+// To ensure other parts of the application can use this class
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle: string  = 'Acme: Product Management';
 }
